5a2b562849f5c2f3f9bfcdc10058c970d6fd87c8
Rick Batra <Batra.Rick@gmail.com>
2013-08-09 23:32:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BreezeDemo::Application.config.secret_token = '1eb5fbc426481cb014e4f6d874410be14ff40dbe7ccd98b2ec195f424c399202d6a9ef489dbc3e258801132601fddbf15f8a565a314f14b90a5d498279153eb4'
