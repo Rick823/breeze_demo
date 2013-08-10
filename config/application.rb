0868c6b571ef4d5359505774109d68df0f929f06
Rick Batra <Batra.Rick@gmail.com>
2013-08-10 03:50:11 -0400

    Disabled the asset pipeline to attempt to fix errors with ExecJS

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module BreezeDemo
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false # disabled due to errors with ExecJS
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
